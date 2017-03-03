2d7ebbb0f9dbdab7d46965c803e7411398957db8
Gordana Minovska <ekogoca@gmail.com>
2017-03-03 15:50:42 -0800

    Apply styles to feed.php

--- feed.php
+++ feed.php
@@ -1,9 +1,24 @@
+<!doctype html>
+<html>
+    
+<head>
+    <meta charset="UTF-8">
+    <title>Feeds List</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="../css/style.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+<header>
+   
 <?php
 
   $request = $_GET['rss'];
   $response = file_get_contents($request);
   $xml = simplexml_load_string($response);
-  print '<h1>' . $xml->channel->title . '</h1>';
+  echo '<h1>' . $xml->channel->title . '</h1>
+        </header>';
+  
 
   //the images are not showing because they are under a namespace "media".
   //get an array of namespace prefixes with their associated URIs.
@@ -14,9 +29,9 @@
     $thumbnail = $story->children($ns['media']);
     //display only stories that have thumbnail images
     if($thumbnail) {
-      echo '<a href="' . $story->link . '">' . $story->title . '</a><br />';
+      echo '<div><h3><a href="' . $story->link . '">' . $story->title . '</a></h3><br />';
       echo '<img src=' .$thumbnail->thumbnail->attributes()->url . '></img>'; 
-      echo '<p>' . $story->description . '</p><br /><br />';
+      echo '<p>' . $story->description . '</p><br /></div>';
     }    
   }
 ?>
