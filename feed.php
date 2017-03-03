a2852e10ab983490ce128298e9cc3ece194480c1
Gordana Minovska <ekogoca@gmail.com>
2017-03-02 22:14:45 -0800

    Show news feed images

--- feed.php
+++ feed.php
@@ -1,14 +1,22 @@
 <?php
-//read-feed-simpleXML.php
-//our simplest example of consuming an RSS feed
-var_dump($_GET['rss']);
+
   $request = $_GET['rss'];
   $response = file_get_contents($request);
   $xml = simplexml_load_string($response);
   print '<h1>' . $xml->channel->title . '</h1>';
+
+  //the images are not showing because they are under a namespace "media".
+  //get an array of namespace prefixes with their associated URIs.
+  //read more@: https://www.sitepoint.com/parsing-xml-with-simplexml/
+  $ns = $xml->getNamespaces(true);
   foreach($xml->channel->item as $story)
   {
-    echo '<a href="' . $story->link . '">' . $story->title . '</a><br />'; 
-    echo '<p>' . $story->description . '</p><br /><br />';
+    $thumbnail = $story->children($ns['media']);
+    //display only stories that have thumbnail images
+    if($thumbnail) {
+      echo '<a href="' . $story->link . '">' . $story->title . '</a><br />';
+      echo '<img src=' .$thumbnail->thumbnail->attributes()->url . '></img>'; 
+      echo '<p>' . $story->description . '</p><br /><br />';
+    }    
   }
 ?>
