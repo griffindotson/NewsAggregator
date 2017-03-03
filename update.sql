5353b6305c0e4f32a5d36621c9ac16bba4afba75
Gordana Minovska <ekogoca@gmail.com>
2017-03-02 20:36:03 -0800

    Update rss feeds for global warming and renewable energy subcategories

---
+++ update.sql
@@ -0,0 +1,7 @@
+UPDATE subcategories
+SET SubcategoryRSS = 'https://rss.sciencedaily.com/earth_climate/global_warming.xml'
+WHERE SubcategoryID = 7;
+
+UPDATE subcategories
+SET SubcategoryRSS = 'https://rss.sciencedaily.com/earth_climate/renewable_energy.xml'
+WHERE SubcategoryID = 9;
